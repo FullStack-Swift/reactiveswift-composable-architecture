14d815590ace3c956dfb2ca684b51b791b2ae55b
PhongND <phongnd@ftech.ai>
2023-09-12 13:23:54 +0700

    update spm

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,30 +10,41 @@ let package = Package(
     .macOS(.v10_15),
     .tvOS(.v13),
     .watchOS(.v6),
+    .visionOS(.v1)
   ],
   products: [
     .library(
       name: "ComposableArchitecture",
-      targets: ["ComposableArchitecture"]),
+      targets: ["ComposableArchitecture"])
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.0.0"),
-    .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "0.5.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.4.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.2.0"),
+    .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "7.1.1"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-collections", from: "1.0.2"),
+    .package(url: "https://github.com/apple/swift-async-algorithms", from: "0.1.0"),
+    .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+    .package(url: "https://github.com/pointfreeco/combine-schedulers", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
   ],
   targets: [
     .target(
       name: "ComposableArchitecture",
       dependencies: [
         "ReactiveSwift",
+        .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
         .product(name: "CasePaths", package: "swift-case-paths"),
         .product(name: "CombineSchedulers", package: "combine-schedulers"),
         .product(name: "CustomDump", package: "swift-custom-dump"),
+        .product(name: "Dependencies", package: "swift-dependencies"),
         .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
+        .product(name: "OrderedCollections", package: "swift-collections"),
+        .product(name: "SwiftUINavigation", package: "swiftui-navigation"),
+        .product(name: "SwiftUINavigationCore", package: "swiftui-navigation"),
         .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]),
     .testTarget(
@@ -43,8 +54,19 @@ let package = Package(
       name: "swift-composable-architecture-benchmark",
       dependencies: [
         "ComposableArchitecture",
-        .product(name: "Benchmark", package: "Benchmark"),
+        .product(name: "Benchmark", package: "swift-benchmark"),
       ]
     ),
   ]
 )
+
+//for target in package.targets {
+//  target.swiftSettings = target.swiftSettings ?? []
+//  target.swiftSettings?.append(
+//    .unsafeFlags([
+//      "-Xfrontend", "-warn-concurrency",
+//      "-Xfrontend", "-enable-actor-data-race-checks",
+//      "-enable-library-evolution",
+//    ])
+//  )
+//}
